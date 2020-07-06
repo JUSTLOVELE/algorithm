6900cfe38d050ca80e9101245bef6d4b9313535a
JUSTLOVELE <694105388@qq.com>
2020-07-06 10:22:57 +0800

    add

--- java/JavaFrame/ActiveMQ/src/main/java/org/yzl/java/frame/activemq/JmsConsumer.java
+++ java/JavaFrame/ActiveMQ/src/main/java/org/yzl/java/frame/activemq/JmsConsumer.java
@@ -1,6 +1,7 @@
 package org.yzl.java.frame.activemq;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.junit.Test;
 
 import javax.jms.*;
 
@@ -10,7 +11,48 @@ public class JmsConsumer {
 
     public static final String QUEUE_NAME = "queue01";
 
-    public static void main(String[] args) {
+    @Test
+    public void connectCase02() {
+
+        try{
+            //1.创建连接工厂,按照给定的url地址,采用默认用户名和密码
+            ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
+            //2.通过连接工场,获得连接connection
+            Connection connection = activeMQConnectionFactory.createConnection();
+            connection.start();
+            //3.创建会话session
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            //4.创建目的地,具体是队列还是主题
+            Queue queue = session.createQueue(QUEUE_NAME);
+            //5.创建消息的消费者
+            MessageConsumer consumer = session.createConsumer(queue);
+            consumer.setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message) {
+
+                    if(message != null && message instanceof TextMessage) {
+
+                        TextMessage textMessage = (TextMessage) message;
+                        try {
+                            System.out.println("消费者接受到消息" + textMessage.getText());
+                        } catch (JMSException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+
+            System.in.read();
+            consumer.close();
+            session.close();
+            connection.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void connectCase01() {
 
         try {
             //1.创建连接工厂,按照给定的url地址,采用默认用户名和密码
@@ -44,6 +86,10 @@ public class JmsConsumer {
         }catch (Exception e) {
             e.printStackTrace();
         }
+
+    }
+
+    public static void main(String[] args) {
 
     }
 }
